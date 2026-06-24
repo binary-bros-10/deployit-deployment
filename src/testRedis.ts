cb218c67b79625d99b262edefa47360817eb0329
Karan Harsh Singh <ask.geminiservices@gmail.com>
2026-06-24 18:29:09 +0530

    Setup redis

--- src/testRedis.ts
+++ src/testRedis.ts
@@ -1,12 +1,16 @@
+import "dotenv/config";
 import { redis } from "./config/redis";
 
 async function test() {
-  await redis.set("test", "hello");
-  const value = await redis.get("test");
+  try {
+    await redis.set("test", "hello");
+    const value = await redis.get("test");
 
-  console.log(value);
-
-  process.exit(0);
+    console.log(value);
+  } finally {
+    redis.disconnect();
+    process.exit(0);
+  }
 }
 
 test();
